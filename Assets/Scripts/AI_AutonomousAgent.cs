f1f2ffda044229a46b561f802a3cd1c357c8adaa
Makenzie Buchenroth <mbuchenroth@student.neumont.edu>
2024-01-09 14:25:02 -0700

    1/9/2024 -- Created agent, movement, perception, autonomous agent, kinematic movement, and distance perception.

---
+++ Assets/Scripts/AI_AutonomousAgent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_AutonomousAgent : AI_Agent {
+    public AI_Perception perception = null;
+
+    private void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/AI_DistancePerception.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_DistancePerception : AI_Perception
+{
+    public override GameObject[] getGameObjects() {
+        throw new System.NotImplementedException();
+    }
+}
