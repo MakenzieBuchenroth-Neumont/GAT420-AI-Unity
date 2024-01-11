f613ddd2de33670811870e0fccc996dd317e7568
Makenzie Buchenroth <mbuchenroth@student.neumont.edu>
2024-01-11 15:24:01 -0700

    1/10/2024 & 1/11/2024

--- Assets/Scripts/AI_DistancePerception.cs
+++ Assets/Scripts/AI_DistancePerception.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AI_DistancePerception : AI_Perception
 {
     public override GameObject[] getGameObjects() {
-        throw new System.NotImplementedException();
+        List<GameObject> result = new List<GameObject>();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, distance);
+        foreach (Collider collider in colliders) {
+            // check if collision is self, skip if so
+            if (collider.gameObject == gameObject) continue;
+            if (tagName == "" || collider.CompareTag(tagName)) {
+                // calculate angle from transform forward vector to direction of game object
+                Vector3 direction = (collider.transform.position - transform.position).normalized;
+                float angle = Vector3.Angle(transform.forward, direction);
+                // if angle is less than max angle, add game object
+                if (angle <= maxAngle) {
+                    result.Add(collider.gameObject);
+                }
+            }
+        }
+
+        return result.ToArray();
     }
 }
--- Assets/Scripts/AI_Perception.cs
+++ Assets/Scripts/AI_Perception.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class AI_Perception : MonoBehaviour {
-    [SerializeField] string tagName = "";
-    [SerializeField] float distance = 1;
-    [SerializeField] float maxAngle = 45;
+    [SerializeField] protected string tagName = "";
+    [SerializeField] protected float distance = 1;
+    [SerializeField] protected float maxAngle = 45;
 
     public string TagName { get { return tagName; } }
     public float Distance { get { return distance; } }
